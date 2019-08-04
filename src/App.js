17fc57b1dbaf043a5a9a14dabea9bd0280029c91
Harsha Meerchandani <Harsha.Meerchandani@in.ey.com>
2019-08-04 12:07:12 +0530

    Post-data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React,{ useState , useEffect } from 'react'
-import axios from "axios" 
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function App() {
-  const [data, setData] = useState({ hits: [] });
-  useEffect(() => {
-  const fetchData = async () => {
-  const result = await axios(
-  'http://hn.algolia.com/api/v1/search?query=redux'
-  );
-  setData(result.data);
-  };
-   fetchData();
-  }, []);
-  console.log(data);
+  const [data, setData] = useState([])
+useEffect(() => {
+const fetchData = async () => {
+const result = await axios.post('https://jsonplaceholder.typicode.com/posts',  {
+  "userId": "harsha",
+  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto" 
+},)
+setData(result.data)
+}
+fetchData()
+}, [])
+console.log(data)
   return (
-    <div>
-      tnthyt
-    </div>
-    
+ //   <div>
+// {data.map(item => (
+// <div key={item.id}>
+// <h1>{item.title}</h1>
+// <h1>{item.title}</h1>
+
+// </div>
+// ))}
+<button />
+//</div>
   )
 }
+
