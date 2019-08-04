687564d45809b943c82dcf76f2917bbd8c314d99
Harsha Meerchandani <Harsha.Meerchandani@in.ey.com>
2019-08-04 12:22:03 +0530

    Get-post-put-del

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ export default function App() {
   const [data, setData] = useState([])
 useEffect(() => {
 const fetchData = async () => {
-const result = await axios.post('https://jsonplaceholder.typicode.com/posts',  {
-  "userId": "harsha",
+const result = await axios.delete('https://jsonplaceholder.typicode.com/posts/1',{
+  "userId": "thehhe",
   "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
   "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto" 
 },)
@@ -14,7 +14,7 @@ setData(result.data)
 }
 fetchData()
 }, [])
-console.log(data)
+console.log(data,"-------------responce from server")
   return (
  //   <div>
 // {data.map(item => (
