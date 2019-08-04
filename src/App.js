4697b2cbee2e2898a3c0d80ec6274b5454623d92
Harsha Meerchandani <Harsha.Meerchandani@in.ey.com>
2019-08-04 14:13:14 +0530

    Component(Togglebutton)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import  React, { useState } from 'react'
-import Component1 from './Component1';
+import Glowing from './Glowing';
 
 export default function App() {
-  const [state, setstate] = useState("Harsha")
+  const [glowing, setGlowing] = useState(false)
   return (
-    <div>
-      <Component1 name="Harsha"></Component1>
-      <Component1 name="1" markpoints="10"></Component1>
-      <Component1 name="2"></Component1>
-      <Component1 name="3"></Component1>
-      <Component1 name="4"></Component1>
-      <Component1 name="5"></Component1>
-      <h1>{ state }</h1>
-      <button onClick={() => setstate("12")}>Click on it</button>
-    </div>
-
-
+      <div> { glowing ? <Glowing name="harsha1"/> : "no" }
+      <button onClick={() => (setGlowing(!glowing))}>Click Me</button>
+      </div>
   )
 }
