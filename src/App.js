7d49355f8153dc828612de1dbf6d803d2c024ff0
Harsha Meerchandani <Harsha.Meerchandani@in.ey.com>
2019-08-04 13:13:54 +0530

    COmponent

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
+import Component1 from './Component1';
 
 export default function App() {
-  const [data, setData] = useState([])
-useEffect(() => {
-const fetchData = async () => {
-const result = await axios.delete('https://jsonplaceholder.typicode.com/posts/1',{
-  "userId": "thehhe",
-  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto" 
-},)
-setData(result.data)
-}
-fetchData()
-}, [])
-console.log(data,"-------------responce from server")
   return (
- //   <div>
-// {data.map(item => (
-// <div key={item.id}>
-// <h1>{item.title}</h1>
-// <h1>{item.title}</h1>
-
-// </div>
-// ))}
-<button />
-//</div>
+    <div>
+      <Component1></Component1>
+    </div>
   )
 }
-
